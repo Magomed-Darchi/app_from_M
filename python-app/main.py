88c532b44441ff04d28989aa0ddae49b8663dc3b
Magomed-Darchi <gorchm93@gmail.com>
2024-02-10 13:39:58 +0300

    python-app from M

---
+++ python-app/main.py
@@ -0,0 +1,8 @@
+import calendar
+print('Добро пожаловать \n')
+year = int(input('Введите год: '))
+month = int(input('Введите месяц: '))
+
+print(calendar.month(year,month))
+
+print(' Всего хорошего') 
